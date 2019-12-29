fc9dcc0b003a33d6689f2f7c158a795183f1f56c
debjit-ray <debjit.tolearn@gmail.com>
2019-12-29 06:01:58 +0530

    Updated the comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,8 @@
 ##    Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a 
 ##    matrix rather than compute it repeatedly The current program contains a pair of functions that 
 ##    cache the inverse of a matrix.
+## Author: Debjit Ray
+## Date: Dec 29, 2019
 ##------------------------------------------------------------------------------------------------------------------
 
 
